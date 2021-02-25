8277d9dbbb0142fa9fb27c9e49a1393e90612d1d
BoraOzcoban <72252068+BoraOzcoban@users.noreply.github.com>
2021-02-25 21:09:54 +0300

    m

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -8,6 +8,5 @@ urlpatterns = [
     path('sort_city', views.SortCity.as_view(), name="sort_city"),
     path('sort_city2', views.SortCity2.as_view(), name="sort_city2"),
     path('sort_city3', views.SortCity3.as_view(), name="sort_city3"),
-    path('cuisine', views.Cuisine.as_view(), name="cuisine"),
     path('cuisine2', views.Cuisine2.as_view(), name="cuisine2"),
 ]
--- restaurant/views.py
+++ restaurant/views.py
@@ -10,7 +10,5 @@ class SortCity2(TemplateView):
     template_name = 'sort_city2.html'
 class SortCity3(TemplateView):
     template_name = 'sort_city3.html'
-class Cuisine(TemplateView):
-    template_name = 'cuisine.html'
 class Cuisine2(TemplateView):
     template_name = 'cuisine2.html'
